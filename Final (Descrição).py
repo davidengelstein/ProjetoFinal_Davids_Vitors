48013ec8a7f703f8ecadaefe21dce1ab207371fd
Vitor Shin Kitahara <viskita96@gmail.com>
2015-05-03 00:26:30 -0300

    Descrição

---
+++ Final (Descrição).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 23:24:57 2015
+
+@author: vitorkitahara
+"""
+
+1.Tinsper
+
+2.David Dayan
+  David Engelstein
+  Vitor Shin
+  Vitor Morozini
+  
+3.- Pessoas que procuram amizades ou relacionamentos novos no universo universitário
+  
+4. a) Sistema de matches
+    O usuário poderá dar like ou dislike, de acordo com as suas preferências.
+    Quando houver o like nos dois lados, os dois poderão conversar.
+   b) Sistema de universidades
+    O usuário poderá escolher com que universidades ele quer interagir.
+
+5.Tinder e Hot or Not
+
+6.Tutoriais do Youtube, Serviço de nuvens do Google, Dicionário, Livros
+
+7.-Jogo onde o usuário deverá controlar um carro, desviando de obstáculos
+  -Jogo onde o usuário irá controlar um personagem que deverá se de obstáculos
+  que irão cair do céu
+    
+    
+    
