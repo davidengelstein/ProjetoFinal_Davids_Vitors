a652dfa1fe7b5cefda700d42f139feaf7c50ced1
vitormorozini <vitor_morozini@hotmail.com>
2015-06-10 22:31:52 -0300

    BOTAO QUIT E RANKING OK

--- Game7.py
+++ Game7.py
@@ -96,7 +96,9 @@ def text_objects(text,cor,size):
     elif size == 'large':
         textSurface = largefont.render(text,True,cor)
     return textSurface,textSurface.get_rect()
-    
+
+
+
 def mensagem (msg,cor,size):
     textSurf, textRect = text_objects(msg,cor,size)    
     textRect.center = (largura_da_tela/2),(altura_da_tela/2)
@@ -117,9 +119,6 @@ def botao(x,y,w,h,ic,ac,acao=None):
                 quit()
             if acao == 'ranking':
                 pass
-
-                
-
     else:
         pygame.draw.rect(DisplayDoJogo, ic,(x,y,w,h))
         
@@ -140,8 +139,8 @@ def intro():
 
                 
         botao(310,400,180,40,black,green,acao = 'play')
-        botao(310,520,180,40,black,red,acao = 'ranking')
-        botao(310,460,180,40,black,yellow,acao = 'quit')
+        botao(310,460,180,40,black,red,acao = 'ranking')
+        botao(310,520,180,40,black,yellow,acao = 'quit')
 
         font = pygame.font.SysFont(None, 40)
         text = font.render('PLAY',True,white)
@@ -165,7 +164,7 @@ def bater(Jogador):
     #pygame.mixer.Sound.play(faustao)
     mensagem('ERROOOOU!!!',red,'large')
     Ranking(Jogador.Score)
-    ordena_ranking(produto)
+    print(produto)
     time.sleep(2)
     #loop_jogo() 
     intro()   
@@ -181,20 +180,7 @@ def Ranking(Score):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None)
         # Escreve dados no Firebase
         fb.put('/', "Scores", produto)
-
-
-def ordena_ranking(produto):     
-    def ordena(dici):
-        if dici == None:
-            return 0
-        for k in dici:
-            return dici[k]   
-    ordem = list(reversed(sorted(produto, key=ordena)))    
-    for cada in ordem:
-        if cada == None:
-            return 0
-        for i,j in cada.items():
-            print(i, " : ", j)
+       
 
 def fundo(x,y):
     DisplayDoJogo.blit(Imagem_Fundo,(x,y))
